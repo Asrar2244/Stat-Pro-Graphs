ad7b730edcc6bd30758360c82478eec23e5afc26
syednoor6208 <syednoor6208@gmail.com>
2024-06-30 22:41:38 +0530

    refactor: welcome page

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export * from './db';
 export * from './home-folders';
 export * from './dock-layout';
 export * from './locale';
+export * from './welcome';
--- src/screens/welcome/index.tsx
+++ src/screens/welcome/index.tsx
@@ -1,11 +1,7 @@
 import { FC } from 'react';
+import { WELCOME_URL } from '@constants';
 
 export const WelcomePage: FC = () => {
   //Todo: need to replace with specified path
-  return (
-    <iframe
-      src="https://pages.github.com"
-      style={{ width: '100%', height: '100%', border: 'none' }}
-    />
-  );
+  return <iframe src={WELCOME_URL} style={{ width: '100%', height: '100%', border: 'none' }} />;
 };
