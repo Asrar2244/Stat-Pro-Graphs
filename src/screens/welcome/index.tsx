51b95741c0d37676cfc2e851970e504c9e9f13da
syednoor6208 <syednoor6208@gmail.com>
2024-06-30 22:42:34 +0530

    refactor: welcome page

--- src/screens/welcome/index.tsx
+++ src/screens/welcome/index.tsx
@@ -2,6 +2,5 @@ import { FC } from 'react';
 import { WELCOME_URL } from '@constants';
 
 export const WelcomePage: FC = () => {
-  //Todo: need to replace with specified path
   return <iframe src={WELCOME_URL} style={{ width: '100%', height: '100%', border: 'none' }} />;
 };
