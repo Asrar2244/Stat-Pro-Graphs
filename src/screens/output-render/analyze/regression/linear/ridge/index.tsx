a27931867f053de8059e6d9bb6abb2f5f2c91f6f
Syed Noor <syednoor6208@gmail.com>
2024-09-06 15:32:40 +0530

    refactor: updated ridge regression and remove everything from input (#11)

--- src/screens/output-render/analyze/regression/linear/ridge/index.tsx
+++ src/screens/output-render/analyze/regression/linear/ridge/index.tsx
@@ -1,28 +1,18 @@
-import { FC, useContext } from 'react';
-import { OutputRenderContext } from '../../../../context';
-import { useGenerator } from './hooks/use-generator';
+import { FC } from 'react';
+
 import { useRegressions } from '../../../../styles-hook/use-regressions-style';
-import { TableCreator } from '@libs';
+
 import { Card, CardHeader, Body1Stronger } from '@fluentui/react-components';
-import { RidgePredictGraph } from './graphs/ridge-predict';
-import ridgeConfig from './configuration/ridge-config.json';
-import { useTranslation } from 'react-i18next';
+
 export const LinearRidgeRegression: FC = () => {
-  const context = useContext(OutputRenderContext);
-  const { tables, tableRef } = useGenerator(context?.selectedRun, ridgeConfig, []);
-  const { t } = useTranslation('reqLinearLeastSquareOutput');
   const classes = useRegressions();
   return (
     <div className={classes.regressionsLayout}>
-      {tables.map((table) => (
-        <Card key={table.name}>
-          {table?.showCaption && (
-            <CardHeader header={<Body1Stronger>{t(table.name)}</Body1Stronger>} />
-          )}
-          <TableCreator {...table} />
-        </Card>
-      ))}
-      {tables.length > 0 && <RidgePredictGraph tableRef={tableRef} />}
+      <Card>
+        <CardHeader header={<Body1Stronger>Header</Body1Stronger>} />
+        <h1>Here table</h1>
+      </Card>
+      <h1>Graph</h1>
     </div>
   );
 };
--- src/screens/top-menu/analyze/regression/linear/ridge/use-ridge-analyze-data.ts
+++ src/screens/top-menu/analyze/regression/linear/ridge/use-ridge-analyze-data.ts
@@ -1,20 +1,18 @@
 import { useShallow } from 'zustand/react/shallow';
 import { useRidge } from './use-ridge-store-hook';
-// import { API } from '@constants';
-// import { useTasks } from '@store/common-msg-store';
+import { API } from '@constants';
 import { collectionsLocation } from '@utils';
+import { useAnalyzeSave } from '@hooks';
 interface IOutputAnalyzeData {
   ridgeAnalyzeData: (tableName: string, tabId: string, queueFor: string, queueType: string) => void;
 }
 export const useRidgeAnalyzeData = (): IOutputAnalyzeData => {
   const { ridge } = useRidge(useShallow((state) => ({ ridge: state })));
-  // const { setQueueTask } = useTasks(useShallow((state) => ({ setQueueTask: state.setQueueTask })));
-
+  const { save } = useAnalyzeSave();
   const ridgeAnalyzeData = async (
     tableName: string,
-    // tabId: string,
-    // queueFor: string,
-    // queueType: string,
+    queueFor: string,
+    queueType: string,
   ): Promise<void> => {
     const db_path = await collectionsLocation(tableName);
     const parameters = {
@@ -33,14 +31,12 @@ export const useRidgeAnalyzeData = (): IOutputAnalyzeData => {
       },
       sub_type: 'ridge',
     };
-    // setQueueTask({
-    //   parameters,
-    //   queueFor,
-    //   queueType,
-    //   tabId,
-    //   tabName: tableName,
-    //   url: `${API.analysis}/api`,
-    // });
+    save(tableName, parameters, {
+      queueFor,
+      url: `/api/${API.analysis}`,
+      method: 'POST',
+      queueType,
+    });
   };
 
   return { ridgeAnalyzeData };
