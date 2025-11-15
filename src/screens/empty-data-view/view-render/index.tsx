31bb057805cba3fab17a3852ddffc0a74c9f1a29
Abha-saxena123 <85979768+Abha-saxena123@users.noreply.github.com>
2025-11-15 21:22:18 +0530

    Feature/update excel package (#153)
    * feat: update excel package
    
    * chore: removed unused variables
    
    ---------
    
    Co-authored-by: Abha-saxena123 <abhasaxena.962007@gmail.com>

--- src/screens/empty-data-view/view-render/index.tsx
+++ src/screens/empty-data-view/view-render/index.tsx
@@ -1,23 +1,19 @@
 import { useEffect, useContext } from 'react';
-import Spreadsheet, { CellBase, Matrix, Selection } from 'react-spreadsheet';
+import { CellBase, Matrix } from 'react-spreadsheet';
 import { DivShowScrollOnHover } from '@libs';
 import { useViewRenderLayout } from '../styles-hook/use-view-render';
 import { EmptyDataContext } from '../context';
-import { ColumnCreate } from './columns';
-import { RowHeaderCreate } from './rows';
 import { useContextMenu } from './hooks/use-context-menu';
 import { ContextMenuComponent } from './context-menu';
 import { ExcelView } from "@excel-view/library";
 
 export const ViewRender = () => {
-  const { data, /*columns,*/ setData, setSelectedCell, setDataState } =
+  const { data, /*columns,*/ setData } =
     useContext(EmptyDataContext);
   const contextMenu = useContextMenu();
   const classes = useViewRenderLayout();
 
-
-
-  const updateData = (noOfRows: number = 50, noOfColumns: number = 10) => {
+  const updateData = (noOfRows: number = 50, noOfColumns: number = 36) => {
     if (setData) {
       const data: Matrix<CellBase> = [];
       for (let i = 0; i < noOfRows; i++) {
@@ -35,57 +31,13 @@ export const ViewRender = () => {
     updateData();
   }, [setData]);
 
-  const onSelectHandler = (cell: Selection) => {
-    if (setSelectedCell) {
-      setSelectedCell(cell as any);
-    }
-  };
 
-  const handleContextMenu = (e: React.MouseEvent, header: string, type: 'row' | 'column') => {
-    e.preventDefault();
-    contextMenu.setType(type);
-    contextMenu.setClickedHeader(header);
-    contextMenu.setAnchorPoint({ x: e.clientX, y: e.clientY });
-    contextMenu.setMenuOpen(true);
-  };
-
-  const handleOnChange = (state: Matrix<CellBase<any>>) => {
-    setDataState?.('draft');
-    setData?.(state);
-  };
-  console.log(data, "===========data")
   return (
     <DivShowScrollOnHover customClass={classes.layoutToolStrip} key={`${JSON.stringify(data)}`}>
-      {/* <Spreadsheet
-        data={data}
-        onChange={handleOnChange}
-        onSelect={onSelectHandler}
-        RowIndicator={(props) => RowHeaderCreate({ ...props, handleContextMenu })}
-        ColumnIndicator={(props) => ColumnCreate({ ...props, handleContextMenu })}
-        darkMode
-      /> */}
       <ExcelView
         showTabs={false}
         showToolbar={true}
         sheets={[{ data } as any]}
-      // sheets={[
-      //   {
-      //     columns: [
-      //       { type: "numeric", title: "Id" },
-      //       { type: "text", width: "350px", title: "Title" },
-      //       { type: "text", width: "250px", title: "Artist" },
-      //       { type: "text", title: "Service" },
-      //       { type: "text", title: "IPO" },
-      //     ],
-      //     data: [
-      //       ["1", "DIVINELY UNINSPIRED TO A HELLISH EXTENT", "LEWIS CAPALDI"],
-      //       ["2", "NO 6 COLLABORATIONS PROJECT", "ED SHEERAN"],
-      //       ["3", "THE GREATEST SHOWMAN", "MOTION PICTURE CAST RECORDING"],
-      //       ["4", "WHEN WE ALL FALL ASLEEP WHERE DO WE GO", "BILLIE EILISH"],
-      //       ["5", "What are there", "Book"],
-      //     ],
-      //   },
-      // ]}
       />
       <ContextMenuComponent {...contextMenu} />
     </DivShowScrollOnHover>
